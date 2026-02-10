ad375485af656fc5d182d7cfe41d756e9d844c4a
Brendan Saliba <brendanbsaliba@gmail.com>
2026-02-10 16:15:40 -0500

    added reserved virual address space

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/mman.h>
 
 #define KiB(n) ((uint64_t)(n) << 10)
 #define MiB(n) ((uint64_t)(n) << 20)
@@ -15,11 +17,13 @@
 #define ARENA_BASE_POS (sizeof(mem_arena))
 #define ARENA_ALIGN (sizeof(void*))
 typedef struct {
-    uint64_t capacity;
+    uint64_t reserve_size;
+    uint64_t commit_size;
     uint64_t pos;
+    uint64_t commit_pos;
 } mem_arena;
 
-mem_arena* arena_create(uint64_t capacity);
+mem_arena* arena_create(uint64_t reserve_size, uint64_t commit_size);
 void arena_destroy(mem_arena* arena);
 void* arena_push(mem_arena* arena, uint64_t size, bool non_zero);
 void arena_pop(mem_arena* arena, uint64_t size);
@@ -31,36 +35,69 @@ void arena_pop_clear(mem_arena* arena);
 #define PUSH_ARRAY(arena, T, n) (T*)arena_push(arena, sizeof(T) * (n), false)
 #define PUSH_ARRAY_NZ(arena, T, n) (T*)arena_push(arena, sizeof(T) * (n), true)
 
+uint32_t plat_get_pagesize(void);
+void* plat_mem_reserve(uint64_t size);
+bool plat_mem_commit(void* ptr, uint64_t size);
+bool plat_mem_decommit(void* ptr, uint64_t size);
+bool plat_mem_release(void* ptr, uint64_t size);
+
 int main(void) {
-    mem_arena* perm_arena = arena_create(MiB(1));
+
+    mem_arena* perm_arena = arena_create(GiB(1), MiB(1));
     printf("Arena created\n");
-    arena_destroy(perm_arena);
-    printf("Arena destroyed\n");
 
     return 0;
 }
 
 
-mem_arena* arena_create(uint64_t capacity) {
-    mem_arena* arena = (mem_arena*)malloc(capacity);
+mem_arena* arena_create(uint64_t reserve_size, uint64_t commit_size) {
+    uint32_t pagesize = plat_get_pagesize();
+    reserve_size = ALIGN_UP_POW2(reserve_size, pagesize);
+    commit_size = ALIGN_UP_POW2(commit_size, pagesize);
 
-    arena->capacity = capacity;
+    mem_arena* arena = plat_mem_reserve(reserve_size);
+
+    if (!plat_mem_commit(arena, commit_size)) {
+        printf("Memory commit failed. Aborting...");
+        abort();
+    }
+
+    arena->reserve_size = reserve_size;
+    arena->commit_size = commit_size;
     arena->pos = ARENA_BASE_POS;
+    arena->commit_pos = commit_size;
 
     return arena;
 }
 
 void arena_destroy(mem_arena* arena) {
-    free(arena);
+    plat_mem_release(arena, arena->reserve_size);
 }
 
 void* arena_push(mem_arena* arena, uint64_t size, bool non_zero) {
     uint64_t pos_aligned = ALIGN_UP_POW2(arena->pos, ARENA_ALIGN);
     uint64_t new_pos = pos_aligned + size;
 
-    if (new_pos > arena->capacity) {
+    if (new_pos > arena->reserve_size) {
         printf("New position is greater than the arena capacity. Aborting...");
         abort();
+    }
+
+    if (new_pos > arena->commit_pos) {
+        uint64_t new_commit_pos = new_pos;
+        new_commit_pos += arena->commit_size - 1;
+        new_commit_pos -= new_commit_pos % arena->commit_size;
+        new_commit_pos = MIN(new_commit_pos, arena->reserve_size);
+
+        uint8_t* mem = (uint8_t*)arena + arena->commit_pos;
+        uint64_t commit_size = new_commit_pos - arena->commit_pos;
+
+        if (!plat_mem_commit(mem, commit_size)) {
+            printf("Memory commit failed. Aborting...");
+            abort();
+        }
+
+        arena->commit_pos = new_commit_pos;
     }
 
     arena->pos = new_pos;
@@ -86,4 +123,35 @@ void arena_pop_to(mem_arena* arena, uint64_t pos) {
 
 void arena_pop_clear(mem_arena* arena) {
     arena_pop_to(arena, ARENA_BASE_POS);
+}
+
+uint32_t plat_get_pagesize(void) {
+    int size = getpagesize();
+    return size;
+}
+
+void* plat_mem_reserve(uint64_t size) {
+    void* out = mmap(NULL, size, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (out == MAP_FAILED) {
+        return NULL;
+    }
+    return out;
+}
+
+bool plat_mem_commit(void* ptr, uint64_t size) {
+    int32_t ret = mprotect(ptr, size, PROT_READ | PROT_WRITE);
+    return ret == 0;
+}
+
+
+bool plat_mem_decommit(void* ptr, uint64_t size) {
+    int32_t ret = mprotect(ptr, size, PROT_NONE);
+    if (ret != 0) return false;
+    ret = madvise(ptr, size, MADV_DONTNEED);
+    return ret == 0;
+}
+
+bool plat_mem_release(void* ptr, uint64_t size) {
+    int32_t ret = munmap(ptr, size);
+    return ret == 0;
 }
